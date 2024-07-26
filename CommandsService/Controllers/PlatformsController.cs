8c42bf67ef8f17016c894ee479f629222a015f8e
Alireza Marhamatabadi <alireza.marhamatabadi@broadangle.com>
2024-07-26 13:28:31 +0300

    Initial CommandService has been done.

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/command/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+                
+        }
+
+        [HttpGet]
+        public IActionResult TestInbound()
+        {
+            Console.WriteLine("Inbound request works on command service....");
+            return Ok("Inbound request works on command service....");
+        }
+    }
+}
